920e95857b9fb3a035e5fc86b28888567c6be30a
ha4219 <ha4219@naver.com>
2024-10-07 13:23:43 +0900

    chore: edit repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/monkey-clone/",
+  base: "/monkeytype-clone/",
   plugins: [react()],
   resolve: {
     alias:{
